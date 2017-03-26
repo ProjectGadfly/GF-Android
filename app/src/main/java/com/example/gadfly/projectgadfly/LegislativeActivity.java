e61bf489918294ad743c525c34ba3873ff2a8662
Papa Kojo Ampim-Darko <papakojoampimdarko@hotmail.com>
2017-03-26 10:38:01 -0500

    Edited layouts and cleaned up code

--- app/src/main/java/com/example/gadfly/projectgadfly/LegislativeActivity.java
+++ app/src/main/java/com/example/gadfly/projectgadfly/LegislativeActivity.java
@@ -111,7 +111,7 @@ public class LegislativeActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
         return true;
     }
 
