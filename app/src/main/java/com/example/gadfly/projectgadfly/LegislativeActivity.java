eb8a71abe60507edbdec92b90981878912a991d8
Papa Kojo Ampim-Darko <papakojoampimdarko@hotmail.com>
2017-03-30 14:31:29 -0500

    Added qr code scanning verification

--- app/src/main/java/com/example/gadfly/projectgadfly/LegislativeActivity.java
+++ app/src/main/java/com/example/gadfly/projectgadfly/LegislativeActivity.java
@@ -58,7 +58,6 @@ public class LegislativeActivity extends AppCompatActivity
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
-
         //Set the layout of the activity based on legislator_activity layout
         setContentView(R.layout.legislator_activity);
 
@@ -152,11 +151,12 @@ public class LegislativeActivity extends AppCompatActivity
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-
+        View contentView = getWindow().findViewById(R.id.content_main);
         if (scanningResult != null) {
             String scanFormat = scanningResult.getFormatName();
             String scanContent = scanningResult.getContents();
-            if (scanContent != null) {
+            String actualurl = "http://gadfly.mobi/services/v1/representatives?address="; //"http://gadfly.mobi/services/v1/script?id="
+            if (scanContent != null && scanContent.startsWith(actualurl)) {
                 ScanResult scanResult = new ScanResult();
                 Bundle bundle = new Bundle();
                 bundle.putString("scanFormat", scanFormat);
@@ -166,12 +166,13 @@ public class LegislativeActivity extends AppCompatActivity
                         .beginTransaction()
                         .add(scanResult, "SCANPAGE")
                         .replace(R.id.content_main, scanResult)
-                        .addToBackStack(null)
-                        .commitAllowingStateLoss();
+                            .commitAllowingStateLoss();
             }
         } else {
             Toast toast = Toast.makeText(getApplicationContext(), R.string.rescan_qr_code, Toast.LENGTH_LONG);
             toast.show();
+            Snackbar.make(contentView, R.string.no_internet, Snackbar.LENGTH_LONG)
+                    .show();
         }
     }
 
@@ -212,19 +213,16 @@ public class LegislativeActivity extends AppCompatActivity
             fragmentManager
                     .beginTransaction()
                     .replace(R.id.content_main, legislatorParsing)
-                    .addToBackStack(null)
                     .commit();
         } else if (id == R.id.about) {
             fragmentManager
                     .beginTransaction()
                     .replace(R.id.content_main, aboutFragment)
-                    .addToBackStack(null)
                     .commit();
         } else if (id == R.id.team) {
             fragmentManager
                     .beginTransaction()
                     .replace(R.id.content_main, team)
-                    .addToBackStack(null)
                     .commit();
         } else if (id == R.id.tutorial) {
             Intent intent = new Intent(getApplicationContext(), Introduction.class);
@@ -248,11 +246,11 @@ public class LegislativeActivity extends AppCompatActivity
     private class JsonTask extends AsyncTask<String, String, String> {
         protected void onPreExecute() {
             super.onPreExecute();
-            progressDialog = new ProgressDialog(getApplicationContext());
-            //Create a dialog when waiting for the activity to execute
-            progressDialog.setMessage("Please wait");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
+//            progressDialog = new ProgressDialog(getApplicationContext());
+//            //Create a dialog when waiting for the activity to execute
+//            progressDialog.setMessage("Please wait");
+//            progressDialog.setCancelable(false);
+//            progressDialog.show();
         }
 
         protected String doInBackground(String... params) {
@@ -302,9 +300,9 @@ public class LegislativeActivity extends AppCompatActivity
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            if (progressDialog.isShowing()){
-                progressDialog.dismiss();
-            }
+//            if (progressDialog.isShowing()){
+//                progressDialog.dismiss();
+//            }
         }
     }
 
