66c829287d0bcba5aee7c9c890689dc21adb67be
Papa Kojo Ampim-Darko <papakojoampimdarko@hotmail.com>
2017-04-30 20:35:00 -0500

    Added usability improvements

--- app/src/main/java/com/forvm/gadfly/projectgadfly/ScriptSuccess.java
+++ app/src/main/java/com/forvm/gadfly/projectgadfly/ScriptSuccess.java
@@ -6,8 +6,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,10 +17,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ShareActionProvider;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 /**
@@ -34,10 +39,10 @@ public class ScriptSuccess extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private ShareActionProvider mShareActionProvider;
 
     private TextView textView;
     private ImageView imageView;
+    private Bitmap bmp;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -53,8 +58,19 @@ public class ScriptSuccess extends Fragment {
         // Inflate the layout for this fragment
         String scriptID = getArguments().getString("scriptID");
         byte[] byteArray = getArguments().getByteArray("image");
-        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         View v = inflater.inflate(R.layout.fragment_script_success, container, false);
+        try {
+
+            File cachePath = new File(getContext().getCacheDir(), "images");
+            cachePath.mkdirs(); // don't forget to make the directory
+            FileOutputStream stream = new FileOutputStream(cachePath + "/image.png"); // overwrites this image every time
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            stream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         textView = (TextView) v.findViewById(R.id.SHOWSCRIPT);
         imageView = (ImageView) v.findViewById(R.id.QRCODE);
         imageView.setImageBitmap(bmp);
@@ -74,33 +90,33 @@ public class ScriptSuccess extends Fragment {
         menu.findItem(R.id.action_submit).setVisible(false);
         MenuItem shareItem = menu.findItem(R.id.action_share);
         shareItem.setVisible(true);
-        mShareActionProvider = (ShareActionProvider) shareItem.getActionProvider();
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT,
-                textView.getText());
-        mShareActionProvider.setShareIntent(shareIntent);
-    }
-
-    private void setShareIntent(Intent shareIntent) {
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(shareIntent);
-        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_share) {
-
+        if (item.getItemId() == R.id.action_share) {
+            shareImage();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    private void shareImage() {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                textView.getText());
+        File imagePath = new File(getContext().getCacheDir(), "images");
+        File newFile = new File(imagePath, "image.png");
+        Uri contentUri = FileProvider.getUriForFile(getContext(), "com.forvm.gadfly.fileprovider", newFile);
+
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
+        shareIntent.setDataAndType(contentUri, getContext().getContentResolver().getType(contentUri));
+        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+        startActivity(Intent.createChooser(shareIntent,"Share via"));
+    }
+    //    private void setShareIntent(Intent shareIntent) {
+//        if (mShareActionProvider != null) {
+//            mShareActionProvider.setShareIntent(shareIntent);
+//        }
+//    }
 }
